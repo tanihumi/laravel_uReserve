eb74457325ae64d198cae21c4dc6466e349a4d0a
tanihumi <x7azzsy@gmail.com>
2023-05-22 21:43:02 +0900

    calendar

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Constant' => App\Constants\EventConst::class,
     ])->toArray(),
 
 ];
